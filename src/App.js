e05626b7833075ee814b34003a9790022710b86a
JashanB <jashan_brar50@hotmail.com>
2021-02-15 17:43:20 -0500

    adding states and passing to children

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import green from './green.jpg';
 import mountain from './mountain.jpg';
 import sky from './sky.jpg';
 import beach from './beach.jpg';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const [transformAmount, setTransformAmount] = useState(-1200);
+  // const carouselSlide = document.querySelector('.carousel-slide');
+  // const carouselImages = document.querySelectorAll('.carousel-slide img');
+  useEffect(()=> {
+    setTransformAmount(state => state * counter);
+  }, [counter]);
+  const styles = {
+    transform: `translateX(${transformAmount}px)`
+  }
+  
+  // carouselSlide.getElementsByClassName.transform = 'translate'
   return (
     <div className="App">
       <div className='carousel-container'>
-        <div className="carousel-slide">
+        <div className="carousel-slide" style={styles}>
+        <img id="last-clone"alt="" src ={beach}></img>
           <img alt="" src ={lake}></img>
+          <img alt="" src ={sky}></img>
           <img alt="" src ={green}></img>
           <img alt="" src ={mountain}></img>
-          <img alt="" src ={sky}></img>
           <img alt="" src ={beach}></img>
+          <img id="first-clone" alt="" src ={lake}></img>
         </div>
       </div>
       <div className="button-container">
