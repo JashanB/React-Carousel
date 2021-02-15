eaeca01d65c3201f2a8fc946f50a462abd68057f
JashanB <jashan_brar50@hotmail.com>
2021-02-15 18:07:07 -0500

    adding style and transistion function

--- src/App.js
+++ src/App.js
@@ -9,39 +9,41 @@ import { useState, useEffect } from 'react';
 function App() {
   const [counter, setCounter] = useState(1);
   const [transformAmount, setTransformAmount] = useState(-1200);
-  useEffect(()=> {
-    console.log('triggered')
-    setTransformAmount(state => -1200 * counter);
+  useEffect(() => {
+    counter <= 0 ? setTransformAmount(state => -1200 * counter) : setTransformAmount(state => -1200 * counter);
+    setTransformAmount(state => -1200 * counter)
   }, [counter]);
   const styles = {
     transform: `translateX(${transformAmount}px)`,
-    transistion: 'transform 0.4s ease-in-out'
+    transition: 'transform 0.4s ease-in-out'
   }
-  
+
   function prevSlide() {
     setCounter(state => state - 1);
   }
   function nextSlide() {
     setCounter(state => state + 1);
-  } 
-  console.log(counter)
-  console.log(transformAmount)
+  }
+  function handleTransitionEnd (event) {
+    console.log('fired')
+  }
+
   return (
-    <div className="App"> 
+    <div className="App">
       <div className='carousel-container'>
-        <div className="carousel-slide" style={styles}>
-        <img id="last-clone"alt="" src ={beach}></img>
-          <img alt="" src ={lake}></img>
-          <img alt="" src ={sky}></img>
-          <img alt="" src ={green}></img>
-          <img alt="" src ={mountain}></img>
-          <img alt="" src ={beach}></img>
-          <img id="first-clone" alt="" src ={lake}></img>
+        <div className="carousel-slide" style={styles}  onTransitionEnd={(event) => handleTransitionEnd(event)}>
+          <img id="last-clone" alt="" src={beach}></img>
+          <img alt="" src={lake}></img>
+          <img alt="" src={sky}></img>
+          <img alt="" src={green}></img>
+          <img alt="" src={mountain}></img>
+          <img alt="" src={beach}></img>
+          <img id="first-clone" alt="" src={lake}></img>
         </div>
       </div>
       <div className="button-container">
-      <button onClick={() => prevSlide()}>Prev</button>
-      <button onClick={() => nextSlide()}>Next</button>
+        <button onClick={() => prevSlide()}>Prev</button>
+        <button onClick={() => nextSlide()}>Next</button>
       </div>
     </div>
   );
