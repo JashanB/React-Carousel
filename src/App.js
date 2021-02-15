b853b1177d95937ef216f40aae687af9fa136569
JashanB <jashan_brar50@hotmail.com>
2021-02-15 17:05:53 -0500

    adding assets and skeleton

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='carousel-container'>
+        <div className="carousel-slide">
+          
+        </div>
+      </div>
     </div>
   );
 }
