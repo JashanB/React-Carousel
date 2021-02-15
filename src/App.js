f6a806b12cf99e06d266c91c76d118f2b7ef07c3
JashanB <jashan_brar50@hotmail.com>
2021-02-15 17:10:48 -0500

    added imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import lake from './lake.jpg';
+import green from './green.jpg';
+import mountain from './mountain.jpg';
+import sky from './sky.jpg';
+import beach from './beach.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className='carousel-container'>
         <div className="carousel-slide">
-          
+          <img alt="" src ={lake}></img>
+          <img alt="" src ={green}></img>
+          <img alt="" src ={mountain}></img>
+          <img alt="" src ={sky}></img>
+          <img alt="" src ={beach}></img>
         </div>
       </div>
     </div>
